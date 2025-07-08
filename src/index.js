a1c67ad78f84759de7a1f2778cfcb21b217d2654
kdilshan5712 <kdilshanbandara5712@gmail.com>
2025-07-08 11:11:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Users from './Users';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/Users' element={<Users />} />
-        
+      <Route path='/' element={<App />} />
+      <Route path='/Users' element={<Users />} />
     </Routes>
   </BrowserRouter>
 );
